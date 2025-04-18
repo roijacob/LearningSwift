4fc47a42aa734224703f9bfc97d9cd1639374f46
roijacob <roijac2b@outlook.com>
2025-04-18 13:04:07 +0800

    Initial Commit

---
+++ SwiftSamples/SwiftSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSamplesApp.swift
+//  SwiftSamples
+//
+//  Created by Roi Jacob on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
