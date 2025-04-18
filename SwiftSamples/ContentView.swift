78096492bf3abfd6424236c2b127061c29c52877
roijacob <roijac2b@outlook.com>
2025-04-18 16:35:14 +0800

    How to create a custom `NavigationLink` view Create a custom NavigationLink view that can be used in NavigationStack.

--- SwiftSamples/ContentView.swift
+++ SwiftSamples/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+struct TeamSeven: Identifiable, Hashable {
+    let id = NSUUID().uuidString
+    let name: String
+}
+
 struct ContentView: View {
+    let members: [TeamSeven] = [
+        .init(name: "Naruto"),
+        .init(name: "Sasuke"),
+        .init(name: "Sakura")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack(root: {
+            VStack(content: {
+                ForEach(members, content: { member in
+                    /// 1. Attach a custom NavigationLink view as the label
+                    NavigationLink(value: member, label: {
+                        ZStack(content: {
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 300, height: 200)
+                            Text(member.name)
+                                .foregroundStyle(.white)
+                        })
+                    })
+                })
+            })
+            .navigationDestination(for: TeamSeven.self, destination: { member in
+                GreetingView(inputMember: member)
+            })
+        })
+    }
+}
+
+struct GreetingView: View {
+    let inputMember: TeamSeven
+    
+    var body: some View {
+        Text("Hello \(inputMember.name)!")
     }
 }
 
