ff4fa25f58d083cf7d96771564c06e0bda06ebb7
roijacob <roijac2b@outlook.com>
2025-07-20 11:10:15 +0800

    Follow Prof. John Gallaugher Ch. 8.1
    Reference: https://www.youtube.com/watch?v=dYk5rrwrOKI

---
+++ SnacktacularUI/SnacktacularUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnacktacularUI
+//
+//  Created by Roi Jacob on 7/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
